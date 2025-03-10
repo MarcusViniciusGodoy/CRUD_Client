d4f93a9617e50fcd79a179c1799e9acb89aea6c2
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-03-10 17:27:36 -0300

    Criada classe client

---
+++ src/test/java/com/desafio3/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio3.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
